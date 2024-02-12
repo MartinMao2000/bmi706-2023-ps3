f616e257266cc6fb0fbf26cf48778e540b2528eb
MartinMao2000 <maojinge@foxmail.com>
2024-02-11 19:36:47 -0500

    1

--- streamlit_app.py
+++ streamlit_app.py
@@ -83,8 +83,9 @@ selected_countries = st.multiselect('Select countries', options=all_countries, d
 
 ### P2.4 ###
 # replace with st.selectbox
-cancer = "Malignant neoplasm of stomach"
-subset = subset[subset["Cancer"] == cancer]
+cancer_types = df['Cancer'].unique().tolist()  # Assuming df is your DataFrame
+selected_cancer = st.selectbox('Select a cancer type', options=cancer_types)  # Cancer type dropdown
+subset = subset[subset["Cancer"] == selected_cancer]
 ### P2.4 ###
 
 
